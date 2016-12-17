6ef190848ed8db687300950a551ee22db9fba0b3
klkwarrior <ossusum.omnination@gmail.com>
2016-12-16 21:18:44 -0500

    Fixed performance issue

--- src/Indexer.java
+++ src/Indexer.java
@@ -8,92 +8,82 @@ import java.util.HashMap;
 
 /**
  * Indexes the Url Links from a crawler/ URL array for ranking
- *
+ * <p>
  * Created by Benny Pena on 12/10/2016.
  */
 public class Indexer {
 
     private ArrayList<String> keywordUrls;
-    private ArrayList<String> allwords;
-    private HashMap<String,ArrayList<String>> hashmap;
+    private HashMap<String, ArrayList<String>> hashmap;
 
     public Indexer() {
         this.keywordUrls = new ArrayList<>();
-        this.allwords = new ArrayList<>();
         this.hashmap = new HashMap<>();
     }
 
-    public void index(){
+    public void index() {
         readUrls();
         getAllWords();
-        System.out.println(allwords.toString());
-        insertToHash();
-        System.out.println(hashmap.toString());
+        writeToFile();
+        System.out.print("DONE!!");
     }
 
-    private void insertToHash() {
-        System.out.println("Total Words To Index: " + allwords.size());
-        for (int i = 0; i < allwords.size();++i){
-            System.out.println("Word Indexing now: " + i);
-            String keyword = allwords.get(i);
-            Document doc = null;
-            Connection connection = null;
-            ArrayList<String> related = new ArrayList<>();
-            for(int j = 0; j < keywordUrls.size();++j){
-                connection = Jsoup.connect(keywordUrls.get(j));
-                try {
-                    doc = connection.get();
 
-                } catch (Exception e) {
-                    doc = null;
-                }
-                if (doc != null){
-                    if(doc.text().contains(keyword)){
-                        related.add(keywordUrls.get(j));
-                    }
-                }
-            }
-            hashmap.put(keyword,related);
-            System.out.println("Finished indexing: " + i);
-        }
-    }
-
-    private void getAllWords(){
-        Document doc = null;
-        Connection connection = null;
-        for (int i = 0; i < keywordUrls.size(); ++i){
+    private void getAllWords() {
+        Document doc;
+        Connection connection;
+        for (int i = 0; i < keywordUrls.size(); ++i) {
             connection = Jsoup.connect(keywordUrls.get(i));
-            System.out.println("Indexing: "+keywordUrls.get(i));
+            System.out.println("Indexing: " + keywordUrls.get(i));
             try {
                 doc = connection.get();
-                System.out.println("Got the Document!");
             } catch (Exception e) {
                 doc = null;
             }
-            if (doc != null){
+            if (doc != null) {
                 String[] documentWords = doc.text().split(" ");
-                for (int j = 0; j < documentWords.length; ++j){
-                    if (!allwords.contains(documentWords[j])){
-                        allwords.add(documentWords[j]);
+                for (int j = 0; j < documentWords.length; ++j) {
+                    if (!hashmap.containsKey(documentWords[j])) {
+                        hashmap.put(documentWords[j], new ArrayList<String>());
                     }
+                    ArrayList<String> urls = hashmap.get(documentWords[j]);
+                    urls.add(keywordUrls.get(i));
+
+
                 }
             }
         }
     }
 
 
-    private void readUrls(){
+    private void readUrls() {
         File readFile = new File("CrawlerURLS.txt");
         try {
             FileReader fReader = new FileReader(readFile);
             BufferedReader bReader = new BufferedReader(fReader);
             String line = "";
-            while((line = bReader.readLine()) != null ){
+            while ((line = bReader.readLine()) != null) {
                 keywordUrls.add(line);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToFile() {
+        File hashTable = new File("Hash.txt");
+        try {
+            FileWriter fWriter = new FileWriter(hashTable);
+            BufferedWriter bWriter = new BufferedWriter(fWriter);
+            for (int i = 0; i < hashmap.size(); ++i) {
+                bWriter.write(hashmap.entrySet().toArray()[i].toString());
+                bWriter.newLine();
+            }
+            bWriter.close();
+            fWriter.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
