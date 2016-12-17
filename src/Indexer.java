e02f5fff10184ce974dac0700804c322ef6f2001
klkwarrior <ossusum.omnination@gmail.com>
2016-12-16 20:50:50 -0500

    Indexer can now index, very slowly

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,5 +1,10 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Indexes the Url Links from a crawler/ URL array for ranking
@@ -7,19 +12,77 @@ import java.util.ArrayList;
  * Created by Benny Pena on 12/10/2016.
  */
 public class Indexer {
-    private Crawler crawler;
+
     private ArrayList<String> keywordUrls;
+    private ArrayList<String> allwords;
+    private HashMap<String,ArrayList<String>> hashmap;
 
-    public Indexer(Crawler crawler) {
-        this.crawler = crawler;
-        keywordUrls = new ArrayList<>();
-
+    public Indexer() {
+        this.keywordUrls = new ArrayList<>();
+        this.allwords = new ArrayList<>();
+        this.hashmap = new HashMap<>();
     }
-    //will index the array of URLs indexed
-    public void index() {
 
+    public void index(){
+        readUrls();
+        getAllWords();
+        System.out.println(allwords.toString());
+        insertToHash();
+        System.out.println(hashmap.toString());
     }
-    private ArrayList<String> readUrls(){
+
+    private void insertToHash() {
+        System.out.println("Total Words To Index: " + allwords.size());
+        for (int i = 0; i < allwords.size();++i){
+            System.out.println("Word Indexing now: " + i);
+            String keyword = allwords.get(i);
+            Document doc = null;
+            Connection connection = null;
+            ArrayList<String> related = new ArrayList<>();
+            for(int j = 0; j < keywordUrls.size();++j){
+                connection = Jsoup.connect(keywordUrls.get(j));
+                try {
+                    doc = connection.get();
+
+                } catch (Exception e) {
+                    doc = null;
+                }
+                if (doc != null){
+                    if(doc.text().contains(keyword)){
+                        related.add(keywordUrls.get(j));
+                    }
+                }
+            }
+            hashmap.put(keyword,related);
+            System.out.println("Finished indexing: " + i);
+        }
+    }
+
+    private void getAllWords(){
+        Document doc = null;
+        Connection connection = null;
+        for (int i = 0; i < keywordUrls.size(); ++i){
+            connection = Jsoup.connect(keywordUrls.get(i));
+            System.out.println("Indexing: "+keywordUrls.get(i));
+            try {
+                doc = connection.get();
+                System.out.println("Got the Document!");
+            } catch (Exception e) {
+                doc = null;
+            }
+            if (doc != null){
+                String[] documentWords = doc.text().split(" ");
+                for (int j = 0; j < documentWords.length; ++j){
+                    if (!allwords.contains(documentWords[j])){
+                        allwords.add(documentWords[j]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void readUrls(){
         File readFile = new File("CrawlerURLS.txt");
         try {
             FileReader fReader = new FileReader(readFile);
@@ -33,7 +96,6 @@ public class Indexer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
     }
 
     public ArrayList<String> getKeywordUrls() {
--- src/MainClient.java
+++ src/MainClient.java
@@ -1,16 +1,16 @@
-import java.io.IOException;
-
 /**
  * Created by Benny Pena on 12/10/2016.
  */
 public class MainClient {
     public static void main(String[]args){
         try {
-            Crawler crawler = new Crawler("https://www.youtube.com/","art");
-            Indexer indexer = new Indexer(crawler);
+            //Crawler crawler = new Crawler("https://www.oldwestbury.edu/","art");
+            //Indexer indexer = new Indexer(crawler);
+            Indexer indexer = new Indexer();
+            indexer.index();
             Ranker ranker = new Ranker(indexer.getKeywordUrls());
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
